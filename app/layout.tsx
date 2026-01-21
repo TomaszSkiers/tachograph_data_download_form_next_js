d21a4a24d2be6367cc8b808a1d5b7707201c7838
Tomasz Skierś <tomasz.skiers@gmail.com>
2026-01-21 10:23:50 +0100

    as sense layout, ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import "./globals.css";
+import Script from 'next/script'; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,15 @@ export default function RootLayout({
   return (
     // suppressHydrationWarning zapobiega błędom przy przełączaniu Dark/Light Mode
     <html lang="pl" suppressHydrationWarning>
+      <head>
+        {/* Skrypt Google AdSense */}
+        <Script
+          id="adsense-init"
+          strategy="afterInteractive" // Ładuje skrypt zaraz po tym, jak strona stanie się interaktywna
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5044844456739196"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`
           ${geistSans.variable} 
