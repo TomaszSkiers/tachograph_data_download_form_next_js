05b90858f6263e466713134ff2da6a174067f66b
Tomasz Skierś <tomasz.skiers@gmail.com>
2026-01-21 10:46:55 +0100

    fix adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import "./globals.css";
-import Script from 'next/script'; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,13 +28,11 @@ export default function RootLayout({
     // suppressHydrationWarning zapobiega błędom przy przełączaniu Dark/Light Mode
     <html lang="pl" suppressHydrationWarning>
       <head>
-        {/* Skrypt Google AdSense */}
-        <Script
-          id="adsense-init"
-          strategy="beforeInteractive" // Ładuje skrypt zaraz po tym, jak strona stanie się interaktywna
+        <script
+          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5044844456739196"
           crossOrigin="anonymous"
-        />
+        ></script>
       </head>
       <body
         className={`
